bbf93f307768e75d87bdaaeee1ebd69344459e62
Pronin Alexander <pronin.alx@gmail.com>
2014-02-12 23:42:31 +0200

    Initial Commit

---
+++ 13ThreadsTestHW04/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  13ThreadsTestHW04
+//
+//  Created by Aleksandr Pronin on 12.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RITAppDelegate class]));
+    }
+}
